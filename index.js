a3f9e1f8a9cec56941f30a0ed0577a7de7b54547
rohan-haque <rohanhaque234@gmail.com>
2023-06-11 23:22:02 +0600

    send user data to the client side

--- index.js
+++ index.js
@@ -32,6 +32,12 @@ async function run() {
     const cartCollection = client.db("photoDB").collection("carts");
 
     // Users collection
+
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray()
+      res.send(result);
+    })
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
