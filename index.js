4ced648dbf8bda4c25f3d12577c28578bdde6666
rohan-haque <rohanhaque234@gmail.com>
2023-06-11 23:59:47 +0600

    make admin system was implemented

--- index.js
+++ index.js
@@ -49,6 +49,18 @@ async function run() {
       res.send(result);
     });
 
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateUser = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updateUser)
+      res.send(result)
+    })
+
     // Classes Collection
     app.get("/classes", async (req, res) => {
       const result = await classCollection.find().toArray();
