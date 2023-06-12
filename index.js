954b8de8df302bd81d0b526e017cdf40be6a212f
rohan-haque <rohanhaque234@gmail.com>
2023-06-12 22:13:47 +0600

    varify jwt

--- index.js
+++ index.js
@@ -10,8 +10,29 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// mongodb connection
+// varify jw token
+const verifyToken = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: "No token provided" });
+  }
 
+  // bearer token
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "No token provided" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
+
+// mongodb connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7kcf4qx.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
